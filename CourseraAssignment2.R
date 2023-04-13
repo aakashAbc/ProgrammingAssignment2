014c49b5eb03e1eaf3290d0eca055a2e18b8726c
aakashAbc <130626647+aakashAbc@users.noreply.github.com>
2023-04-13 08:42:06 +0530

    Create CourseraAssignment2.R

---
+++ CourseraAssignment2.R
@@ -0,0 +1,29 @@
+# Create a special "matrix" object that can cache its inverse
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+# Compute the inverse of the special "matrix" returned by makeCacheMatrix above
+cacheSolve <- function(x, ...) {
+  # Return the inverse if it has already been calculated
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  # Compute the inverse of the matrix and cache it
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInverse(inv)
+  inv
+}
